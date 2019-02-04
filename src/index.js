50150e75b281bf502a69fda08865fa9f311722a1
Kevin Brock <kevin.brock@hotmail.com>
2019-02-03 21:22:13 -0800

    updated with circular button render

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 import RadialRender from "./RadialRender";
 
-const Btn = props => {
+const CircleBnt = props => {
   return (
     <div>
-      <button style={{ height: "30px", width: "30px", borderRadius: "15px" }}>
+      <button
+        style={{
+          height: "30px",
+          width: "30px",
+          borderRadius: "15px",
+          border: "none",
+          backgroundColor: "#125ee1",
+          color: "#fefefe"
+        }}
+      >
         {props.children}
       </button>
     </div>
@@ -16,20 +25,23 @@ const Btn = props => {
 
 function App() {
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{ position: "absolute", left: "50px", top: "50px" }}
+    >
       <RadialRender
         sides={5}
         r={70}
         strokeWidth={2}
         components={[
-          <Btn>1</Btn>,
-          <Btn>2</Btn>,
-          <Btn>3</Btn>,
-          <Btn>4</Btn>,
-          <Btn>5</Btn>,
-          <Btn>6</Btn>,
-          <Btn>7</Btn>,
-          <Btn>8</Btn>
+          <CircleBnt>1</CircleBnt>,
+          <CircleBnt>2</CircleBnt>,
+          <CircleBnt>3</CircleBnt>,
+          <CircleBnt>4</CircleBnt>,
+          <CircleBnt>5</CircleBnt>,
+          <CircleBnt>6</CircleBnt>,
+          <CircleBnt>7</CircleBnt>,
+          <CircleBnt>8</CircleBnt>
         ]}
       />
     </div>
