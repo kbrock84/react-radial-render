052a65b66f7eb1e2ef4c3c651c16f9fc3df2e81d
Kevin Brock <kevin.brock@hotmail.com>
2019-02-09 10:10:21 -0800

    removed unused imports

--- src/RadialRender/RadialRender.js
+++ src/RadialRender/RadialRender.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { getPoints } from "./get-points";
-import { red } from "ansi-colors";
 
 class RadialRender extends Component {
   constructor(props) {
