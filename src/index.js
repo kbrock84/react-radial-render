96aa2caa3f810683726229dd5e6defecdbb098dc
Kevin Brock <kevin.brock@hotmail.com>
2019-02-16 16:15:02 -0800

    added control bar

--- src/index.js
+++ src/index.js
@@ -4,12 +4,46 @@ import ErrorBoundary from "react-error-boundary";
 import "./styles.css";
 import RadialRender from "../package-publish/RadialRender";
 
+const ControlBtn = props => {
+  return (
+    <button
+      onClick={props.onClick}
+      style={{
+        borderRadius: "4px",
+        border: "none",
+        backgroundColor: "#333333",
+        color: "#fefefe",
+        margin: "4px",
+        fontSize: "16px",
+        padding: "8px"
+      }}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+const ControlBar = props => {
+  return (
+    <div
+      style={{
+        margin: "4px",
+        padding: "4px",
+        border: "2px solid #888888",
+        borderRadius: "4px"
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
+
 const CircleBnt = props => {
   return (
     <button
       style={{
-        height: "20px",
-        width: "20px",
+        height: "30px",
+        width: "30px",
         borderRadius: "15px",
         border: "none",
         backgroundColor: "#125ee1",
@@ -23,24 +57,57 @@ const CircleBnt = props => {
 
 function App() {
   const [radius, setRadius] = useState(50);
+  const [btn1Text, setBtn1Text] = useState(1);
+  const [childComponents, setChildComponents] = useState([
+    <CircleBnt>{btn1Text}</CircleBnt>,
+    <CircleBnt>2</CircleBnt>,
+    <CircleBnt>3</CircleBnt>,
+    <CircleBnt>4</CircleBnt>,
+    <CircleBnt>5</CircleBnt>
+  ]);
+  const [childLength, setChildLength] = useState(childComponents.length);
+
+  const addChild = () => {
+    let c = childComponents;
+    c.push(
+      <CircleBnt key={childComponents.length - 1}>
+        {childComponents.length + 1}
+      </CircleBnt>
+    );
+    setChildComponents(c);
+    setChildLength(childLength + 1);
+  };
+
+  const removeChild = () => {
+    let c = childComponents;
+    c.pop();
+    setChildComponents(c);
+    setChildLength(childLength - 1);
+  };
+
+  const updateBtn1Text = () => {
+    let components = childComponents;
+    components[0] = <CircleBnt>{btn1Text + 1}</CircleBnt>;
+    setChildComponents(components);
+    setBtn1Text(btn1Text + 1);
+  };
+
   return (
-    <div
-      className="App"
-      style={{ position: "absolute", left: "50px", top: "50px" }}
-    >
-      <button onClick={() => setRadius(radius - 10)}>-</button>
-      <button onClick={() => setRadius(radius + 10)}>+</button>
+    <div className="App">
+      <ControlBar>
+        <ControlBtn onClick={() => updateBtn1Text()}>btn1+</ControlBtn>
+        <ControlBtn onClick={() => setRadius(radius - 10)}>r-</ControlBtn>
+        <ControlBtn onClick={() => setRadius(radius + 10)}>r+</ControlBtn>
+        <ControlBtn onClick={addChild}>components +</ControlBtn>
+        <ControlBtn onClick={removeChild}>components -</ControlBtn>
+      </ControlBar>
       <ErrorBoundary>
-        <RadialRender
-          r={radius}
-          components={[
-            <CircleBnt>1</CircleBnt>,
-            <CircleBnt>2</CircleBnt>,
-            <CircleBnt>3</CircleBnt>,
-            <CircleBnt>4</CircleBnt>,
-            <CircleBnt>5</CircleBnt>
-          ]}
-        />
+        {console.log(childComponents.length)}
+        <RadialRender r={radius}>
+          {childComponents.map((c, i) => (
+            <div key={i}>{c}</div>
+          ))}
+        </RadialRender>
       </ErrorBoundary>
     </div>
   );
