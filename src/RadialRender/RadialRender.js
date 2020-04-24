0c34ad8fafb8e11ccef7b59ce1a533c99977a30c
Kevin <kevin.brock@lcb.state.nv.us>
2020-04-24 09:50:19 -0700

    fixed null child refs

--- src/RadialRender/RadialRender.js
+++ src/RadialRender/RadialRender.js
@@ -1,82 +1,79 @@
-import React, { useLayoutEffect, useState, useRef } from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
+const rChildClass = "react-radial-render-child";
 
 function RChild(props) {
-  const ref = useRef();
-  props.addRef(ref);
-  return (
-    <div
-      ref={ref}
-      style={{
-        position: "absolute",
-        top: `${props.top}px`,
-        left: `${props.left}px`
-      }}
-    >
-      {props.children}
-    </div>
-  );
+	return (
+		<div
+			className={rChildClass}
+			style={{
+				position: "absolute",
+				top: `${props.top}px`,
+				left: `${props.left}px`,
+			}}
+		>
+			{props.children}
+		</div>
+	);
 }
 
 function RadialRender(props) {
-  const containerRef = useRef();
-  const childArray = React.Children.toArray(props.children);
+	const containerRef = useRef();
+	const childArray = React.Children.toArray(props.children);
 
-  const RChildrenRefs = [];
-  const { points, leastX, leastY, greatestX, greatestY } = getPoints(
-    props.r,
-    childArray.length
-  );
-  const addRef = ref => RChildrenRefs.push(ref);
+	const { points, leastX, leastY, greatestX, greatestY } = getPoints(
+		props.r,
+		childArray.length
+	);
 
-  useLayoutEffect(() => {
-    let rightBounds = 0;
-    let bottomBounds = 0;
+	useLayoutEffect(() => {
+		const container = containerRef.current;
+		let rightBounds = 0;
+		let bottomBounds = 0;
 
-    RChildrenRefs.forEach(ref => {
-      let el = ref.current;
+		const childNodes = container.getElementsByClassName(rChildClass);
 
-      const [left, top] = [
-        parseInt(el.style.left.replace("px", "")),
-        parseInt(el.style.top.replace("px", ""))
-      ];
+		for (let i = 0; i < childNodes.length; i++) {
+			const child = childNodes[i];
+			const [left, top] = [
+				parseInt(child.style.left.replace("px", "")),
+				parseInt(child.style.top.replace("px", "")),
+			];
 
-      if (left == greatestX - leastX) {
-        rightBounds = left + el.offsetWidth + leastX;
-      }
+			if (left == greatestX - leastX) {
+				rightBounds = left + child.offsetWidth + leastX;
+			}
 
-      if (top == greatestY - leastY) {
-        bottomBounds = top + el.offsetHeight + leastY;
-      }
-    });
+			if (top == greatestY - leastY) {
+				bottomBounds = top + child.offsetHeight + leastY;
+			}
+		}
 
-    let container = containerRef.current;
-    container.style.width = rightBounds - leastX + "px";
-    container.style.height = bottomBounds - leastY + "px";
-  });
+		container.style.width = rightBounds - leastX + "px";
+		container.style.height = bottomBounds - leastY + "px";
+	});
 
-  return (
-    <div
-      ref={containerRef}
-      className="radial-render-container"
-      style={{ margin: "0", padding: "0", position: "relative" }}
-    >
-      {points.map((point, i) => (
-        <RChild
-          addRef={addRef}
-          key={props.genKey ? props.genKey() : `radial-render-${i}`}
-          top={point.y - leastY}
-          left={point.x - leastX}
-        >
-          {childArray[i]}
-        </RChild>
-      ))}
-    </div>
-  );
+	return (
+		<div
+			ref={containerRef}
+			className="radial-render-container"
+			style={{ margin: "0", padding: "0", position: "relative" }}
+		>
+			{points.map((point, i) => (
+				<RChild
+					key={props.genKey ? props.genKey() : `radial-render-${i}`}
+					top={point.y - leastY}
+					left={point.x - leastX}
+				>
+					{childArray[i]}
+				</RChild>
+			))}
+		</div>
+	);
 }
 
 RadialRender.propTypes = {
-  r: PropTypes.number.isRequired
+	r: PropTypes.number.isRequired,
 };
 
 export default RadialRender;
